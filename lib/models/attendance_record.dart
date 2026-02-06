45c62fd84adf7d49344db798db6b132a920ccb58
Adeodatus Nkundimana <a.nkundiman@alustudent.com>
2026-02-06 16:20:30 +0200

    defined a model class for an attendance record

---
+++ lib/models/attendance_record.dart
@@ -0,0 +1,12 @@
+class AttendanceRecord {
+  final String sessionTitle;
+  final DateTime date;
+  final bool isPresent;
+
+  AttendanceRecord({
+    required this.sessionTitle,
+    required this.date,
+    required this.isPresent,
+  });
+}
+
